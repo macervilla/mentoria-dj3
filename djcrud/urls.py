f8d39a1ac50cab76bafe3f2246afa9050d301843
macervilla <macervilla1@gmail.com>
2026-02-09 19:10:41 -0300

    primera subida1

---
+++ djcrud/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for djcrud project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tasks import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', views.signup,name='signup'),
+    path('tasks/', views.tasks,name='tasks'),
+    path('tasks_completas/', views.tasks_completas,name='tasks_completas'),
+    path('tasks/create/', views.create_task,name='create_task'),
+    path('tasks/<int:task_id>/', views.task_detail,name='task_detail'),
+    path('tasks/<int:task_id>/complete', views.task_complete,name='task_complete'),
+    path('tasks/<int:task_id>/delete', views.task_delete,name='task_delete'),
+    path('logout/', views.logout,name='logout'),
+    path('signin/', views.signin,name='signin'),
+    path('',views.home,name='home')
+]
---
+++ tasks/views.py
@@ -0,0 +1,121 @@
+from django.shortcuts import render,redirect,get_object_or_404
+#from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login,logout,authenticate
+from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def home(request):
+    #return HttpResponse('<h1>hhhh</h1>')
+    #return render(request,'signup.html')
+    title='hola mundo'
+    return render(request,'home.html')
+
+@login_required
+def tasks (request):
+   # tasks=Task.objects.all()
+    tasks=Task.objects.filter(user=request.user,datecompleted__isnull=True)
+    return render(request,'tasks.html',{'tasks':tasks})
+
+@login_required
+def tasks_completas (request):
+   # tasks=Task.objects.all()
+    tasks=Task.objects.filter(user=request.user,datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request,'tasks.html',{'tasks':tasks})
+
+@login_required
+def task_complete(request,task_id):
+     task=get_object_or_404(Task,pk=task_id,user=request.user)
+     if request.method=='POST':
+        task.datecompleted=timezone.now()
+        task.save()
+        return redirect('tasks')
+
+@login_required
+def task_delete(request,task_id):
+     task=get_object_or_404(Task,pk=task_id,user=request.user)
+     if request.method=='POST':
+        task.delete()
+        return redirect('tasks')
+
+@login_required   
+def task_detail(request,task_id):
+        if request.method=='GET':
+            #task=Task.objects.get(pk=task_id)
+            task=get_object_or_404(Task,pk=task_id,user=request.user )
+            form=TaskForm(instance=task)
+            return render(request,'task_detail.html',{'task':task,'form':form})
+        else:
+            try:
+                  task=get_object_or_404(Task,pk=task_id,user=request.user)
+                  form=TaskForm(request.POST,instance=task)
+                  form.save()
+                  return redirect('tasks')
+            except ValueError:
+                    return render(request,'task_detail.html',{'task':task,'form':form,'error':'error'})
+
+                    
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request,'signin.html',{'form':AuthenticationForm})
+    else:
+        #print(request.POST)
+        user=authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request,'signin.html',{'form':AuthenticationForm,'error':'usuario o pass incorrecto'})
+        else:
+            login(request,user)
+            return redirect('tasks')
+@login_required        
+def create_task(request):
+    if request.method == 'GET':
+        return render(request,'create_task.html',{'form':TaskForm})      
+    else:
+            try:
+                form=TaskForm(request.POST)
+                new_task=form.save(commit=False)
+                new_task.user=request.user
+                new_task.save()
+                return redirect('tasks')
+            except ValueError:
+                return render(request,'create_task.html',{
+                    'form':TaskForm,'error':'revisar datos'
+                })
+
+def signup(request):
+    #return HttpResponse('<h1>hhhh</h1>')
+    #return render(request,'signup.html')
+    title='hola mundo'
+    
+    if request.method =='GET':
+      #  print('enviando formulario')
+      return render(request,'signup.html',
+                  {'mytitle':title,'form':UserCreationForm
+                   })
+    else:
+      #  print(request.POST)
+       # print('obteniendo datos')
+        if request.POST['password1'] == request.POST['password2']:
+            #register user
+            try:
+                user=User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('tasks')
+                #return HttpResponse('user created successfully')
+            except:
+                return HttpResponse('user already exists')    
+        return HttpResponse('pass do not match')
+        
+    
