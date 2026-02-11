ab22d59272ffb62b186c78657ffd42a588b14118
macervilla <macervilla1@gmail.com>
2026-02-11 13:06:15 -0300

    first commi4

--- djcrud/settings.py
+++ djcrud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,13 +80,13 @@ WSGI_APPLICATION = 'djcrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-import dj_database_url
+}'''
+
 
 DATABASES = {
     'default': dj_database_url.config(
